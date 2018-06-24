0e2010e41bcf24c977c188a2b34cf22f676f1b26
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2018-06-24 17:27:08 -0300

    Starting commit. Adding maven structure. Adding Token and Tokenizer classes.

---
+++ src/main/java/com/larregle/core/Token.java
@@ -0,0 +1,46 @@
+package com.larregle.core;
+
+public class Token {
+
+    private int index;
+    private int precedence;
+    private TokenType tokenType;
+    private String text;
+
+
+    public int getPrecedence() {
+        return precedence;
+    }
+
+    public void setPrecedence(int precedence) {
+        this.precedence = precedence;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public TokenType getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(TokenType tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public static enum TokenType {
+        NUMBER, FUNCTION, PLUS, MINUS, MULTIPLY, DIVIDE, OPEN_BRACE, CLOSE_BRACE
+    }
+}
---
+++ src/main/java/com/larregle/core/Tokenizer.java
@@ -0,0 +1,84 @@
+package com.larregle.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokenizer {
+
+    private static final Tokenizer instance;
+    private int index;
+
+    static {
+        instance = new Tokenizer();
+    }
+
+    private Tokenizer() { this.index = 0; }
+
+    public static Tokenizer getInstance() { return instance; }
+
+    public List<Token> parse(String expression) {
+        List<Token> result = new ArrayList<>();
+
+        while (index < expression.length()) {
+            if (Character.isWhitespace(expression.charAt(index))) { index++; continue; }
+            result.add(getToken(expression));
+            index++;
+        }
+
+        return result;
+    }
+
+    private Token getToken(String expression) {
+        int startPosition = index;
+        char c = expression.charAt(startPosition);
+        Token token = new Token();
+        token.setIndex(startPosition);
+        token.setText(new String(new char[]{c}));
+
+        switch (c) {
+            case '(': {
+                token.setTokenType(Token.TokenType.OPEN_BRACE);
+                break;
+            }
+            case ')': {
+                token.setTokenType(Token.TokenType.CLOSE_BRACE);
+                break;
+            }
+            case '+': {
+                token.setTokenType(Token.TokenType.PLUS);
+                break;
+            }
+            case '-': {
+                token.setTokenType(Token.TokenType.MINUS);
+                break;
+            }
+            case '/': {
+                token.setTokenType(Token.TokenType.DIVIDE);
+                break;
+            }
+            case '*': {
+                token.setTokenType(Token.TokenType.MULTIPLY);
+                break;
+            }
+            default:
+                if (Character.isDigit(c)) {
+                    if (expression.length() > (index + 1)) {
+                        while (Character.isDigit(expression.charAt(index + 1)) || expression.charAt(index + 1) == '.') {index++;}
+                    }
+                    token.setTokenType(Token.TokenType.NUMBER);
+                    token.setText(expression.substring(startPosition, index + 1));
+                    break;
+                }
+                else if (Character.isAlphabetic(c)) {
+                    if (expression.length() > (index + 1)) {
+                        while (Character.isAlphabetic(expression.charAt(index + 1))) {index++;}
+                    }
+                    token.setText(expression.substring(startPosition, index + 1));
+                    token.setTokenType(Token.TokenType.FUNCTION);
+                    break;
+                }
+        }
+
+        return token;
+    }
+}
